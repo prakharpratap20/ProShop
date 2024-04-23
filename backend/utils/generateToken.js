1cee7de03554bf48c7535befd078338e796246e7
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-04-24 00:21:17 +0530

    done with backend auth

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  // set jwt as HTTP-only cookie
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "developement",
+    sameSite: "strict",
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30days
+  });
+};
+
+export default generateToken;
